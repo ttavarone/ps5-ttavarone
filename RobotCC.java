ad3934cb78cf698ebc5db474edb9181762a54938
Tucker Tavarone <ttavarone@Tuckers-MacBook-Air.local>
2018-03-22 20:28:06 -0400

    Added a Dynamic Programming implementation of the RobotCC and some println statements for data analysis

--- RobotCC.java
+++ RobotCC.java
@@ -1,4 +1,5 @@
-
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 /**
  * Recursive Exhaustive Search of Robot Coin Collection Problem
  *
@@ -7,34 +8,88 @@
  */
 public class RobotCC
 {
-    public int robotCoinCollection( int r, int c, int[][] C)
+    static int comparisonsRecDyn = 0;
+    static int comparisonsRec = 0;
+    public int robotCoinCollection( int r, int c, int[][] C, int[][] S)
     {
+        if(S[r][c] != -1){
+            comparisonsRecDyn++;
+            return S[r][c];
+        }
         if(r == C.length-1 && c == C[0].length-1){
-            return C[r][c];
+            comparisonsRecDyn++;
+            S[r][c]= C[r][c];
+            return S[r][c];
         }
         if(r == C.length-1){
-            return robotCoinCollection(r, c+1, C)+C[r][c];
+            comparisonsRecDyn++;
+            S[r][c] = robotCoinCollection( r, c+1, C, S )+C[r][c];
+            return S[r][c];
         }
         else if(c == C[0].length-1){
-            return robotCoinCollection(r+1, c, C)+C[r][c];
+            comparisonsRecDyn++;
+            S[r][c] = robotCoinCollection(r+1, c, C, S)+C[r][c];
+            return S[r][c];
         }
         else{
-            return Math.max(robotCoinCollection(r+1, c, C),
-                robotCoinCollection(r, c+1, C))+C[r][c];
+            S[r][c] = Math.max(robotCoinCollection(r+1, c, C, S),
+                robotCoinCollection(r, c+1, C, S))+C[r][c];
+            return S[r][c];
         }    
     }
 
-    public static void main(String[] args)
+    public int robotCoinCollectionR( int r, int c, int[][] C)
     {
-        int[][] A = new int[4][4];
+
+        if(r == C.length-1 && c == C[0].length-1){
+            comparisonsRec++;
+            return C[r][c];
+        }
+
+        if(r == C.length-1){
+            comparisonsRec++;
+            return robotCoinCollectionR(r, c+1, C)+C[r][c];
+        }
+
+        else if(c == C[0].length-1){
+            comparisonsRec++;
+            return robotCoinCollectionR(r+1, c, C)+C[r][c];
+        }
+        else{
+
+            return Math.max(robotCoinCollectionR(r+1, c, C),
+                robotCoinCollectionR(r, c+1, C))+C[r][c];
+        }    
+    }
+
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        int n = 8;
+        int[][] A = new int[n][n];
+        int[][] S = new int[n][n];
         for(int i = 0; i < A.length; i++){
             for(int j = 0; j < A[0].length; j++){
-                A[i][j] = 1;
+                A[i][j] = 0;
+                S[i][j] = -1;
             }
         }
+        A[0][0] = 1;
+        A[1][1] = 1;
+        A[2][2] = 1;
+        A[3][3] = 1;
+        A[4][4] = 1;
+        A[5][5] = 1;
+        //A[6][6] = 1;
+        //A[7][7] = 1;
+        //A[8][8] = 1;
+        //A[9][9] = 1;
         
-
         RobotCC test = new RobotCC();
-        System.out.println(test.robotCoinCollection(0,0,A));
+        System.out.println(test.robotCoinCollection(0,0,A,S));
+        System.out.println(test.robotCoinCollectionR(0,0,A));
+
+        System.out.println("n:   "+n);
+        System.out.println("Exhaustive Recursive RCC:   "+comparisonsRec);
+        System.out.println("DynProg. Recursive RCC:   "+comparisonsRecDyn);
     }
 }
